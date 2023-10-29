6ccf980faaa2c05ed5a59c55abe1c68e152b9d18
Thiago Melo Delgado Azevedo <thiagomdazevedo@gmail.com>
2023-10-29 16:19:35 -0300

    API Decoding

---
+++ decoding.py
@@ -0,0 +1,16 @@
+def decoding(QM, ch1, ch2, SG, w):
+    '''
+    QM: (Graph/Matrix) Quantum Hardware
+    ch1: ps(q_i, q_j) (Lista de duplas indicando as portas)
+    ch2: {n_k, n_l} (Lista de duplas indicando os qubits do hardware)
+    SG: Solução parcial do round anterior
+    w: lista de pesos das portas (Talvez fique separado)
+    '''
+    SGr=SG
+    n = range(ch1)
+    #for i in n:
+    
+
+
+    return SGr
+
