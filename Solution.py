e57789c606e5bc65ca8cb84d678162d9557faf70
Raquel <raquelsilveira31415@gmail.com>
2024-03-06 15:54:27 -0300

    Add class Solution

---
+++ Solution.py
@@ -0,0 +1,17 @@
+
+
+
+class Solution:
+    '''
+    Contém ch1, ch2, Grafo, qubitmap e SG?
+    Atualiza o qubitmap e o makespan de cada qubit toda vez que receber um SG atualizado
+    '''
+
+    def __init__(self) -> None:
+        pass
+
+    def makespan(self):
+
+        return 
+    
+    def optimize_time():
