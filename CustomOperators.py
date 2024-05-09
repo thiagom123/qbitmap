0d8f33bcd8480b1b01afc0c9ba63649973b690a4
Raquel <raquelsilveira31415@gmail.com>
2024-05-09 00:14:31 -0300

    wip new mutation

--- CustomOperators.py
+++ CustomOperators.py
@@ -39,7 +39,7 @@ class QAOAPMX(Crossover):
         # define the crossover: number of parents and number of offsprings
         super().__init__(2, 2)
 
-    def _do(self, problem, X, **kwargs):
+    def _do(self, problem, X):
 
         # The input of has the following shape (n_parents, n_matings, n_var)
         _, n_matings, _ = X.shape
@@ -109,70 +109,45 @@ class QAOAPMX(Crossover):
             Y[0, k, 0], Y[1, k, 0] = "".join(off_1), "".join(off_2)
 
         return Y
-
-
-class MyPMX(Crossover):
-
-    def __init__(self):
-
-        # define the crossover: number of parents and number of offsprings
-        super().__init__(2, 2)
-
-    def _do(self, X):
-        # The input has the following shape (n_parents, n_matings, n_var) -> Might change this
-        _, n_matings, n_var = X.shape
-        # In this case, there are 2 parents.
-
-        num_gates = len(X[0,0,0][0])
-        n = int(num_gates/2)
-
-        for k in range(n_matings):
-
-            # get the first and the second parent
-            p1, p2 = X[0, k, 0], X[1, k, 0]
-
-            indexes_p1 = rnd.sample(range(num_gates), n)
-            indexes_p2 = rnd.sample(range(num_gates), n)
-
-            of11 = [0] * num_gates
-            of21 = [0] * num_gates
-            of12 = [0] * num_gates
-            of22 = [0] * num_gates
-
-            # Fill in selected elements for p1
-            for i in indexes_p1:
-                if i < len(p1[0]):
-                    of11[i] = p1[0][i]
-                if i < len(p1[1]):
-                    of12[i] = p1[1][i]
-
-            # Fill in selected elements for p2
-            for i in indexes_p2:
-                if i < len(p2[0]):
-                    of21[i] = p2[0][i]
-                if i < len(p2[1]):
-                    of22[i] = p2[1][i]
-
     
-    
-class MyMutation(Mutation):
+class QAOAMutation(Mutation):
     def __init__(self):
         super().__init__()
 
-    def _do(self, problem, X, **kwargs):
+    def _do(self, problem, X):
 
-        num_gates = len(X[0,0,0][0])
+        # probable Y shape: (pop,1), most likekly offspring pop
+        Y = np.full_like(X, None, dtype=object)
+        num_gates = len(problem.ps_gates)
 
         # for each individual
-        for k in range(len(X)):
+        for i in range(len(X)):
+
+            individual = X(i)
+            ch1 = individual.ch1
+            ch2 = individual.ch2
+
             # Single mutation stage (mut1)
             indexes = rnd.sample(range(num_gates), 2)
-            v1, v2 = X[0,k,indexes[0]][0], X[0,k,indexes[0]][1]
-            X[0,k,indexes[0]][0] = X[0,k,indexes[1]][0]
-            X[0,k,indexes[0]][1] = X[0,k,indexes[1]][1]
-            X[0,k,indexes[1]][0] = v1
-            X[0,k,indexes[1]][2] = v2
+
+            l11, l12 = ch1[indexes[0]], ch2[indexes[0]]
+
+            ch1[indexes[0]] = ch1[indexes[1]]
+            ch2[indexes[0]] = ch2[indexes[2]]
+            ch1[indexes[1]] = l11
+            ch2[indexes[1]]= l12
+
+            # Second mutation stage (mut2)
+
+
+            # put new mutated ind in Y
+            Y[i] = QAOAindividual(ch1, ch2)
             
-            # ch2 mutation (mut2) stage
+
+
+            
+
+        return X
+
     
 #class MyDuplicateElimination()
